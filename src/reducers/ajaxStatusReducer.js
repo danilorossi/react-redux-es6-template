2335e34940e8c30a2ab15f16478ea0475d3b720a
Danilo <contact@danilorossi.me>
2017-08-16 13:26:50 +0200

    Store integration test example

--- src/reducers/ajaxStatusReducer.js
+++ src/reducers/ajaxStatusReducer.js
@@ -5,7 +5,7 @@ function actionTypeEndsInSuccess(type) {
   return type.substring(type.length - 8) == '_SUCCESS';
 }
 
-export default function ajaxStatusReducer(state = initialState.numAjaxCallsInProgress, action) {
+export default function ajaxStatusReducer(state = initialState.ajaxCallsInProgress, action) {
 
   if(action.type == types.BEGIN_AJAX_CALL) {
     return state + 1;
---
+++ src/store/store.test.js
@@ -0,0 +1,34 @@
+import expect from 'expect';
+import { createStore } from 'redux';
+import rootReducer from '../reducers';
+import initialState from '../reducers/initialState';
+import * as courseActions from '../actions/courseActions';
+
+// integration test for redux store
+// - action creators
+// - reducers
+// - store
+
+describe('Store', () => {
+
+  it('should handle creating courses', () => {
+    // arrange
+    const store = createStore(rootReducer, initialState);
+    const course = {
+      title: "Clean Code"
+    };
+
+    // act
+    const action = courseActions.createCourseSuccess(course);
+    store.dispatch(action);
+
+    // assert
+    const actual = store.getState().courses[0];
+    const expected = {
+      title: 'Clean Code'
+    };
+    expect(actual).toEqual(expected);
+
+  });
+
+});
