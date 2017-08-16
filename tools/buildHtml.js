1e4387f8b1cc7e177dcbf36dae121a8e175ea59a
Danilo <contact@danilorossi.me>
2017-08-16 14:01:04 +0200

    dist index.html generator script

---
+++ tools/buildHtml.js
@@ -0,0 +1,25 @@
+import fs from 'fs';
+import cheerio from 'cheerio';
+import colors from 'colors';
+
+/*eslint-disable no-console*/
+
+fs.readFile('src/index.html', 'utf8', (err, markup) => {
+  if(err) {
+    return console.log(err);
+  }
+  const $ = cheerio.load(markup);
+
+  // since a separate CSS is only utilized for a production build,
+  // need to dynamcally ...
+  $('head').prepend('<Link rel="stylesheet" href="styles.css">');
+
+  fs.writeFile('dist/index.html', $.html(), 'utf8', function(err) {
+    if(err) {
+      return console.log(err);
+    }
+    console.log('index.html written to /dist'.green);
+  });
+});
+
+
